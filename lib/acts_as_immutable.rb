b30dca04cd4a5410eaea147bf44d1b648bfe8b54
Simon Mathieu <simon.math@gmail.com>
2012-03-19 16:38:16 -0700

    Immutable models cannot be destroyed

--- lib/acts_as_immutable.rb
+++ lib/acts_as_immutable.rb
@@ -15,16 +15,8 @@ module ActsAsImmutable
   
   module ClassMethods
     def acts_as_immutable(*mutable_attributes, &condition)
-      if mutable_attributes.empty? && !condition.nil?
-        raise StandardError, "the condition block requires a list of mutable attributes"
-      end
-      
-      #init_hook_methods
-      
       immutable_write_attributes(*mutable_attributes, &condition)
-      clear_mutable_condition_after_save
     end
-    
     
     private
 
@@ -34,39 +26,37 @@ module ActsAsImmutable
         # Hook the write_attribute method which is used to set an attribute in ActiveRecord
         # to use our immutability check first
         hook_method(:write_attribute) do |attr_name, value|
-          @mutable_condition ||= instance_eval(&condition) if !condition.nil?
+          mutable_condition = instance_eval(&condition) if !condition.nil?
           
           # Allow attributes to be written if they have not changed,
           # required when associations are saved but not changed
           value_changed = read_attribute(attr_name) != value
-          if new_record? || !value_changed || (mutable_attributes.include?(attr_name.to_s) && @mutable_condition)
+          if new_record? || !value_changed || (mutable_attributes.include?(attr_name.to_s) && mutable_condition)
             _write_attribute(attr_name, value)
           else
             errors.add(attr_name, "is an immutable attribute")
             raise ActiveRecord::ActsAsImmutableError, attr_name
           end
         end
-      end
-      
-      #def setup_hook_methods
-      #  alias :_write_attribute :write_attribute
-      #  alias :_after_save :after_save
-      #end
-      
-      # Clear the mutable condition after a save
-      def clear_mutable_condition_after_save
-        hook_method(:after_save) do
-          @mutable_condition = nil
-          _after_save
+
+        hook_method(:destroy, :orig_destroy) do ||
+          mutable_condition = instance_eval(&condition) if !condition.nil?
+
+          if new_record? || mutable_condition
+            orig_destroy
+          else
+            raise ActiveRecord::ActsAsImmutableError
+          end
         end
       end
       
       # Hook into a method by renaming the current method to _METHOD
       # and define the new method by a block
-      def hook_method(method_name, &block)
+      def hook_method(method_name, new_method = nil, &block)
+        new_method ||= "_#{method_name}"
         ub_original_method = instance_method(method_name)
         undef_method(method_name)
-        define_method "_#{method_name}", ub_original_method
+        define_method new_method, ub_original_method
         define_method method_name, &block
       end
 
@@ -75,8 +65,12 @@ end
 
 module ActiveRecord
   class ActsAsImmutableError < ActiveRecord::ActiveRecordError
-    def initialize(attribute)
-      super("#{attribute} is an immutable attribute")
+    def initialize(attribute = nil) 
+      if attribute
+        super("#{attribute} is an immutable attribute")
+      else
+        super "Model is immutable"
+      end
     end
   end
 end
--- test/cases/test_acts_as_immutable.rb
+++ test/cases/test_acts_as_immutable.rb
@@ -15,6 +15,39 @@ class ActsAsImmutableUsingVirtualField < ActiveRecord::TestCase
       self.record_locked = true
     end
   end
+
+  class Payment2 < ActiveRecord::Base
+    set_table_name :payments
+    attr_accessor :record_locked
+
+    acts_as_immutable do
+      !record_locked
+    end
+
+    def after_initialize
+      self.record_locked = true
+    end
+  end
+
+  def test_writing_attributes_without_list
+    p = Payment.create!(:customer => "Valentin", :status => "success", :amount => 5.00)
+    assert_raises ActiveRecord::ActsAsImmutableError do
+      p.customer = 'test'
+    end
+  end
+
+  def test_destroy
+    p = Payment.create!(:customer => "Valentin", :status => "success", :amount => 5.00)
+    assert_raises ActiveRecord::ActsAsImmutableError do
+      p.destroy
+    end
+
+    p.record_locked = false
+    p.destroy
+
+    p = Payment.new(:customer => "Valentin", :status => "success", :amount => 5.00)
+    p.destroy
+  end
   
   def test_creating_object_directly
     p = Payment.create!(:customer => "Valentin", :status => "success", :amount => 5.00)
